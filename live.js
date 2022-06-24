3610b9f9135ccbe51e842d2a922b6ced194bf05d
Adeola Joseph <104514834+Pentpope@users.noreply.github.com>
2022-06-24 18:12:26 +0100

    Add files via upload

---
+++ live.js
@@ -0,0 +1,26 @@
+let  operation = window.prompt("Choose which operation you would like to perform                                    E.g +,-,÷,* ");
+let  num1 = window.prompt("Enter First no ");
+let num2 = window.prompt("Enter your second no");
+operation == ( '+,-,*, ÷');
+num1 == parseFloat(num1);
+num2 == parseFloat(num2);
+if
+(operation == '+' ) {
+  parseInt()
+   document.write(num1 + num2);
+}
+else if
+(operation == '-' ) {
+document.write(num1 - num2) 
+}
+else if 
+(operation == '*' ) {
+document.write(num1 * num2) 
+}
+else if 
+  (operation == '÷' ) {
+document.write(num1 / num2)
+  }
+else {
+document.write("invalid input") 
+}
